a196f7638bbbc150107e0455a465c32a78ac4f38
Ross <ross.tienken99@gmail.com>
2018-03-14 12:54:24 -0600

    pre-Accordian push

--- ReactNative/src/reducers/SearchReducer.js
+++ ReactNative/src/reducers/SearchReducer.js
@@ -4,15 +4,15 @@ import { filtBeer, filtBrew, labs, zipCs, filtBrewNames } from '../../default'
 
 const INITIAL_STATE = {
   zipCode: '',
-  zipCodes: zipCs,
+  zipCodes: '',
   error: '',
   loading: false,
-  filteredBeers: filtBeer,
-  filteredBreweries: filtBrew,
-  filteredLocal: filtBrew,
+  filteredBeers: '',
+  filteredBreweries: '',
+  filteredLocal: '',
   searchText: '',
   labels: labs,
-  breweryNames: filtBrewNames
+  breweryNames: ''
 }
 
 export default (state = INITIAL_STATE, action) => {
@@ -24,14 +24,13 @@ export default (state = INITIAL_STATE, action) => {
     case SEARCH_CHANGED:
     return { ...state, searchText: action.payload }
     case ZIP_CHANGED:
-    console.log(action.payload)
     return { ...state, zipCode: action.payload }
     case SEARCH_BEERS:
     return { ...state, filteredBeers: action.payload.beersFiltered, labels: action.payload.labels, breweryNames: action.payload.brewNames, searchText: '' }
     case SEARCH_BREWERIES:
-    return { ...state, filteredBeers: action.payload.beersFiltered, filteredBreweries: action.payload.breweriesFiltered, labels: action.payload.labels, breweryNames: action.payload.brewNames, searchText: '' }
+    return { ...state, filteredBreweries: action.payload.breweriesFiltered, searchText: '' }
     case SEARCH_LOCAL:
-    return { ...state, filteredLocal: action.payload.localFiltered, labels: action.payload.labels }
+    return { ...state, filteredLocal: action.payload.localFiltered }
     default:
      return state
   }
